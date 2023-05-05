d405301bbf9c9b6075a2a99977a12e8aea7dedcf
Kallista Xue <ax2170@columbia.edu>
2023-05-05 13:45:46 -0400

    DB setup

--- some_columbia_student_sql.sql
+++ some_columbia_student_sql.sql
@@ -20,6 +20,5 @@ create table columbia_student
  Example insert statement. Note that since auto_id is auto-increment, the insert
  statement does not need to specify it.
  */
- insert into columbia_student (last_name, first_name, middle_name, email, uni)
-    values ("Ferguson", "Donald", "Francis", "dff@cs.columbia.edu", "dff9");
+
 
